e13d072cd6522c49dbeaaea428b9071b581b9bee
francisco-ca <francisco4_1996@hotmail.com>
2020-09-24 22:45:15 +0000

    listo

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from './components/Jumbotron';
 import Card from './components/Card';
 import Navbar from './components/Navbar';
--- landingpage/src/components/Jumbotron.jsx
+++ landingpage/src/components/Jumbotron.jsx
@@ -9,7 +9,7 @@ function Jumbotron(props){
   <hr className="my-4"/>
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
   <p className="lead">
-    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
   </p>
      </div>
     )
