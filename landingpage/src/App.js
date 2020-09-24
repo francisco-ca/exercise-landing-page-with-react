4da830504877841ec78d4454c5aaaeba13b778fb
francisco-ca <francisco4_1996@hotmail.com>
2020-09-24 22:23:52 +0000

    landing page finished

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from './components/Jumbotron';
-import Card from './components/Card'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Card from './components/Card';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-
 <div className= "evth"> 
 <Navbar></Navbar>
 <div className= "container">
